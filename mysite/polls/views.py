9893505af6abc8773d547bf6155fcc70f77c318b
Ricardo Perez <rxperez12@gmail.com>
2024-06-17 16:03:07 -0700

    added detail view function

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 # from django.template import loader
 
 from .models import Question
@@ -15,7 +15,15 @@ def index(request):
 
 def detail(request, question_id):
     """Displays a question text, with no results but with a form to vote."""
-    return HttpResponse("You're looking at question %s." % question_id)
+    # NOTE: one way to write it
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, "polls/detail.html", {"question": question})
+    # NOTE: shortcut way
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):

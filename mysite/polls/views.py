c809fd88c69a53231adda5e5b12ac73bce7756f3
Ricardo Perez <rxperez12@gmail.com>
2024-06-17 17:16:42 -0700

    added generic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,36 +1,57 @@
+from typing import Any
 from django.db.models import F
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 # from django.template import loader
 
 from .models import Question, Choice
 
-
-def index(request):
-    """Displays the latest few questions."""
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    # below returns an HttpResponse object of the given template rendered with given context
-    return render(request, "polls/index.html", context)
-
-
-def detail(request, question_id):
-    """Displays a question text, with no results but with a form to vote."""
-    # NOTE: one way to write it
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    # return render(request, "polls/detail.html", {"question": question})
-    # NOTE: shortcut way
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+# HARD WAY TO WRITE VIEW
+# def index(request):
+#     """Displays the latest few questions."""
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = {"latest_question_list": latest_question_list}
+#     # below returns an HttpResponse object of the given template rendered with given context
+#     return render(request, "polls/index.html", context)
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+# def detail(request, question_id):
+#     """Displays a question text, with no results but with a form to vote."""
+#     # NOTE: one way to write it
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#     # return render(request, "polls/detail.html", {"question": question})
+#     # NOTE: shortcut way
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question})
+
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": question})
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
